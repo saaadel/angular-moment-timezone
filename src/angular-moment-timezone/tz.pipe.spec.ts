785fc612e85c9a8186b90904fe5833a559a827ba
Andrew Spoda <saaadel+git@gmail.com>
2017-12-11 20:10:03 +0300

    fix for angular 5.x.x

--- src/angular-moment-timezone/tz.pipe.spec.ts
+++ src/angular-moment-timezone/tz.pipe.spec.ts
@@ -1,9 +1,10 @@
-// // Removed
-// import {TzPipe} from "./tz.pipe";
+import {TzPipe} from "./tz.pipe";
+// import "jasmine/index";
 
-// describe('TzPipe', () => {
-//   it('create an instance', () => {
-//     const pipe = new TzPipe();
-//     expect(pipe).toBeTruthy();
-//   });
-// });
+
+describe("TzPipe", () => {
+  it("create an instance", () => {
+    const pipe = new TzPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-import './polyfills.PhantomJS';
+import "./polyfills.PhantomJS";
 
 import "zone.js/dist/long-stack-trace-zone";
 import "zone.js/dist/proxy.js";
@@ -24,7 +24,7 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+const context = require.context("./", true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
 // Finally, start Karma to run the tests.
