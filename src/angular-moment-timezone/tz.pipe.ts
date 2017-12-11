f05f6e323e97523ae9b8ec44d981d19273b926e4
Andrew Spoda <saaadel+git@gmail.com>
2017-12-11 23:23:01 +0300

    possible fix systemjs

--- src/angular-moment-timezone/tz.pipe.ts
+++ src/angular-moment-timezone/tz.pipe.ts
@@ -1,8 +1,9 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import * as moment from 'moment-timezone';
+import {Pipe, PipeTransform} from "@angular/core";
+import * as momentFromTz from "moment-timezone";
+import * as moment from "moment";
 
 @Pipe({
-  name: 'amTz'
+  name: "amTz"
 })
 export class TzPipe implements PipeTransform {
 
@@ -12,13 +13,17 @@ export class TzPipe implements PipeTransform {
    * <ul>
    * <li>{{ '2007-03-01T13:00:00Z' }} (raw: TZ - UTC)</li>
    * <li>{{ '2007-03-01T13:00:00Z' | amDateFormat }} (formatted: in current TZ, parse in UTC) = 2007-03-01T16:00:00+03:00</li>
-   * <li>{{ '2007-03-01T13:00:00Z' | amTz:"America/New_York":false | amDateFormat }} (formatted: in America/NY TZ, parse in UTC ) = 2007-03-01T08:00:00-05:00</li>
-   * <li>{{ '2007-03-01T13:00:00Z' | amTz:"America/New_York":true | amDateFormat }} (formatted: in America/NY TZ, parse in UTC) = 2007-03-01T08:00:00-05:00</li>
+   * <li>{{ '2007-03-01T13:00:00Z' | amTz:"America/New_York":false | amDateFormat }} (formatted: in America/NY TZ, parse in UTC )
+   *      = 2007-03-01T08:00:00-05:00</li>
+   * <li>{{ '2007-03-01T13:00:00Z' | amTz:"America/New_York":true | amDateFormat }} (formatted: in America/NY TZ, parse in UTC)
+   *      = 2007-03-01T08:00:00-05:00</li>
    * <li>----</li>
    * <li>{{ '2007-03-01T13:00:00' }} (raw: TZ - Unknown )</li>
    * <li>{{ '2007-03-01T13:00:00' | amDateFormat }} (formatted: in current TZ, parse in current TZ) = 2007-03-01T13:00:00+03:00</li>
-   * <li>{{ '2007-03-01T13:00:00' | amTz:"America/New_York":false | amDateFormat }} (formatted: in America/NY TZ, parse in current TZ) = 2007-03-01T05:00:00-05:00</li>
-   * <li>{{ '2007-03-01T13:00:00' | amTz:"America/New_York":true | amDateFormat }} (formatted: in America/NY TZ, parse in America/NY TZ) = 2007-03-01T13:00:00-05:00</li>
+   * <li>{{ '2007-03-01T13:00:00' | amTz:"America/New_York":false | amDateFormat }} (formatted: in America/NY TZ, parse in current TZ)
+   *      = 2007-03-01T05:00:00-05:00</li>
+   * <li>{{ '2007-03-01T13:00:00' | amTz:"America/New_York":true | amDateFormat }} (formatted: in America/NY TZ, parse in America/NY TZ)
+   *      = 2007-03-01T13:00:00-05:00</li>
    * </ul>
    * @param {moment.MomentInput} value date/time/timestamp value - can be with TimeZone id or shift, or without it. <p/>
    *    See <a href="https://en.wikipedia.org/wiki/ISO_8601">wikipedia article</a> for most usable string formats. <p/>
@@ -29,13 +34,15 @@ export class TzPipe implements PipeTransform {
    *                        See <a href="https://en.wikipedia.org/wiki/Tz_database#Examples">wikipedia examples</a> <p/>
    * @param {boolean} parseInZone <p/>
    *                              If TRUE - parse value (without TZ id or shift) in defined TZData (Parse mode). <p/>
-   *                              If FALSE - convert value (without TZ id or shift) to defined TZData (Conversion mode), and parse it in current TZ. <p/>
-   *                              NOTE: If moment value has TZ id or shift part - Parse mode will be used and "parseInZone" will be ignored. <p/>
+   *                              If FALSE - convert value (without TZ id or shift) to defined TZData (Conversion mode), and parse
+   *                              it in current TZ. <p/>
+   *                              NOTE: If moment value has TZ id or shift part - Parse mode will be used and "parseInZone"
+   *                              will be ignored. <p/>
    * @returns {any} new moment value for defined TZData
    */
   transform(value: moment.MomentInput, tzdata: string, parseInZone: boolean = false): any {
     if (parseInZone) {
-      return moment.tz(value, tzdata);
+      return momentFromTz.tz(value, tzdata);
     }
     return moment(value).tz(tzdata);
   }
--- src/demo/demo.component.ts
+++ src/demo/demo.component.ts
@@ -1,27 +1,35 @@
-import {Component} from '@angular/core';
-import * as moment from 'moment-timezone';
-import {Moment} from 'moment-timezone';
+import {Component} from "@angular/core";
+import * as moment from "moment-timezone";
+import {Moment} from "moment-timezone";
 
 @Component({
-  selector: 'demo-root',
+  selector: "demo-root",
   template: `
     <h1>Demo</h1>
     <div>{{ '2007-03-01T13:00:00Z' }} (raw: TZ - UTC)</div>
     <div>{{ '2007-03-01T13:00:00Z' | amDateFormat }} (formatted: in current TZ, parse in UTC) = 2007-03-01T16:00:00+03:00</div>
-    <div>{{ '2007-03-01T13:00:00Z' | amTz:"America/New_York":false | amDateFormat }} (formatted: in America/NY TZ, parse in UTC ) = 2007-03-01T08:00:00-05:00</div>
-    <div>{{ '2007-03-01T13:00:00Z' | amTz:"America/New_York":true | amDateFormat }} (formatted: in America/NY TZ, parse in UTC) = 2007-03-01T08:00:00-05:00</div>
+    <div>{{ '2007-03-01T13:00:00Z' | amTz:"America/New_York":false | amDateFormat }} (formatted: in America/NY TZ, parse in UTC )
+      = 2007-03-01T08:00:00-05:00
+    </div>
+    <div>{{ '2007-03-01T13:00:00Z' | amTz:"America/New_York":true | amDateFormat }} (formatted: in America/NY TZ, parse in UTC)
+      = 2007-03-01T08:00:00-05:00
+    </div>
     <hr/>
     <div>{{ '2007-03-01T13:00:00' }} (raw: TZ - Unknown )</div>
     <div>{{ '2007-03-01T13:00:00' | amDateFormat }} (formatted: in current TZ, parse in current TZ) = 2007-03-01T13:00:00+03:00</div>
-    <div>{{ '2007-03-01T13:00:00' | amTz:"America/New_York":false | amDateFormat }} (formatted: in America/NY TZ, parse in current TZ) = 2007-03-01T05:00:00-05:00</div>
-    <div>{{ '2007-03-01T13:00:00' | amTz:"America/New_York":true | amDateFormat }} (formatted: in America/NY TZ, parse in America/NY TZ) = 2007-03-01T13:00:00-05:00</div>
+    <div>{{ '2007-03-01T13:00:00' | amTz:"America/New_York":false | amDateFormat }} (formatted: in America/NY TZ, parse in current TZ)
+      = 2007-03-01T05:00:00-05:00
+    </div>
+    <div>{{ '2007-03-01T13:00:00' | amTz:"America/New_York":true | amDateFormat }} (formatted: in America/NY TZ, parse in America/NY TZ)
+      = 2007-03-01T13:00:00-05:00
+    </div>
     <hr/>
     <div>{{ currentMoment | amDateFormat }}</div>
     <div>{{ currentMoment | amTz:"America/New_York" | amDateFormat }}</div>
     <div>{{ currentDate | amTz:"America/New_York" | amDateFormat }}</div>
     <div>{{ currentDateISOString | amTz:"America/New_York" | amDateFormat }}</div>
   `,
-  styles: ['']
+  styles: []
 })
 export class DemoComponent {
   public currentMoment: Moment = moment();
